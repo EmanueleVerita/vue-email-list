dbd8bf635a3b57e210908147fc1e358b5ce223ba
EmanueleVerita <emanuele.verita@gmail.com>
2023-07-06 15:19:28 +0200

    finito

--- js/script.js
+++ js/script.js
@@ -5,17 +5,21 @@ createApp({
     data(){
 
         return{
+             emails : []
+        }
+    },
+    created(){
+        for(let i = 0 ; i < 10 ; i++){
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then((res) =>{
+                
+                console.log(res.data.response)
 
+                this.emails.push(res.data.response)
+            });
+           
+            
         }
     }
 }).mount('#app')
 
-for(let i = 0 ; i < 10 ; i++){
-    axios.get('')
-    .then(function(response){
-        const result = response.data
-        console.log(response.data.response)
-    });
-
-    
-}
